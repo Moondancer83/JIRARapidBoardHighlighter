798d3540c5e0371ea466166b66908fd4be5ccdd8
Mark Kalinovits <kalee.mark@gmail.com>
2017-01-30 11:56:12 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         JIRA Rapid Board highlighter
+// @namespace    http://kalee.hu
+// @version      0.0.1
+// @description  Highlight closed stories on JIRA Rapid Board.
+// @author       Moondancer83
+// @homepage     https://github.com/Moondancer83/JIRARapidBoardHighlighter
+// @include      /jira.*/secure/RapidBoard.jspa*/
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var head = $("head");
+    head.append("<style>.js-issue.ghx-issue-compact.ghx-done {background-color: #98FB98}</style>");
+})();
