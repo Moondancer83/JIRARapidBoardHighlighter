3f0cd4fad7e4e8476f542a2f46f3d8dc7f1b30da
Mark Kalinovits <kalee.mark@gmail.com>
2017-01-31 15:35:04 +0100

    Update and rename index.js to JIRARapidBoardHighlighter.js

--- JIRARapidBoardHighlighter.js
+++ JIRARapidBoardHighlighter.js
@@ -5,8 +5,6 @@
 // @description  Highlight stories on JIRA Rapid Board.
 // @author       Moondancer83
 // @homepage     https://github.com/Moondancer83/JIRARapidBoardHighlighter
-// @downloadURL  https://github.com/Moondancer83/JIRARapidBoardHighlighter/blob/master/index.js
-// @updateURL    https://github.com/Moondancer83/JIRARapidBoardHighlighter/blob/master/index.js
 // @include      /jira.*/secure/RapidBoard.jspa*/
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
