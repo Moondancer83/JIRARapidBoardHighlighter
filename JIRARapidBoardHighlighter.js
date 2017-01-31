552588f90d3eb877ed2a121792f3c74097646050
Mark Kalinovits <kalee.mark@gmail.com>
2017-01-31 15:36:42 +0100

    Update JIRARapidBoardHighlighter.js

--- JIRARapidBoardHighlighter.js
+++ JIRARapidBoardHighlighter.js
@@ -2,9 +2,11 @@
 // @name         JIRA Rapid Board highlighter
 // @namespace    http://www.kalee.hu
 // @version      0.0.1
-// @description  Highlight stories on JIRA Rapid Board.
+// @description  Highlight closed stories on JIRA Rapid Board.
 // @author       Moondancer83
 // @homepage     https://github.com/Moondancer83/JIRARapidBoardHighlighter
+// @downloadURL  https://github.com/Moondancer83/JIRARapidBoardHighlighter/blob/master/index.js
+// @updateURL    https://github.com/Moondancer83/JIRARapidBoardHighlighter/blob/master/index.js
 // @include      /jira.*/secure/RapidBoard.jspa*/
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
@@ -19,14 +21,17 @@
     };
     var head = $("head");
     var username = $("#header-details-user-fullname").data('username');
-    
+
     setTimeout(function() {
         setStyles();
         markMyIssues();
-    }, 5000);
-    
+    }, 2000);
+
     function setStyles() {
-        head.append("<style>.js-issue.ghx-issue-compact.ghx-done {background-color:" + colors.done+ "} .js-issue.ghx-issue-compact.ghx-issue-my {background-color: " + colors.my + "}</style>");    
+        head.append("<style>" +
+                    ".js-issue.ghx-issue-compact.ghx-done {background-color:" + colors.done+ "} " +
+                    ".js-issue.ghx-issue-compact.ghx-issue-my {background-color: " + colors.my + "}" +
+                    "</style>");
     }
     function markMyIssues() {
         $(".ghx-end img[src*=" + username +"].ghx-avatar-img").closest(".js-issue:not('.ghx-done')").addClass("ghx-issue-my");
